2822114d760729f92098a62c9a4689dea9ca30fc
suedarby <sue.a.darby@gmail.com>
2022-09-30 21:09:47 -0800

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashComponent } from './dash/dash.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'dash', component: DashComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      DashComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
